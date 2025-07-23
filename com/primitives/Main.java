43058ea6a164096c7affdea3608617155eaf4028
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-07-24 01:51:56 +0200

    Add files via upload
    Committing Main class

---
+++ com/primitives/Main.java
@@ -0,0 +1,25 @@
+// Java program to practice java primitive integer type and passing value
+
+package com.primitives;
+
+// Main class
+public class Main {
+
+    // Main class
+    public static void main(String[] args) {
+
+        // Creating and initializing local variable
+        int integer_variable = 8;
+
+        // Calling method change
+        change(integer_variable);
+
+        // Printing value of integer_variable to console
+        System.out.println(integer_variable); // Output: 8
+    }
+
+    // Method change (int number) to demonstrate passing value to outer variable
+    public static void change(int number){
+        number = 11;
+    }
+}
